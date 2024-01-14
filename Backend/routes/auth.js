de62ff460a37014ae2fadb68f9abac241ef0d373
lynettenyambura <linetnyambura45@gmail.com>
2024-01-15 00:17:00 +0300

    Userroutes-backend

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerUser,loginUser,forgotPassword,logout, resetPassword } = require('../controllers/authController');
+const { registerUser,
+    loginUser,
+    forgotPassword,
+    resetPassword ,
+    getUserProfile,
+    updatePassword,
+    updateProfile,
+    logout,
+    allUsers,
+    getUserDetails,
+    updateUser,
+    deleteUser
+    
+    
+} = require('../controllers/authController');
+
+const { isAuthenticatedUser ,authorizeRoles} = require('../middlewares/auth');
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
@@ -11,5 +27,13 @@ router.route('/password/reset/:token').put(resetPassword);
 
 router.route('/logout').get(logout);
 
+router.route('/me').get(isAuthenticatedUser, getUserProfile);
+router.route('/password/update').put(isAuthenticatedUser, updatePassword);
+router.route('/me/update').put(isAuthenticatedUser, updateProfile);
+
+router.route('/admin/users').get(isAuthenticatedUser, authorizeRoles('admin'),allUsers);
+router.route('/admin/user/:id').get(isAuthenticatedUser, authorizeRoles('admin'),getUserDetails);
+router.route('/admin/user/:id').put(isAuthenticatedUser, authorizeRoles('admin'),updateUser);
+router.route('/admin/user/:id').delete(isAuthenticatedUser, authorizeRoles('admin'),deleteUser);
 
 module.exports = router;
